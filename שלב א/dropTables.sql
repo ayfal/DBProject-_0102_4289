d4ee9f5404cf0a31bc203b26afde5403ec7b3d16
roei97 <116275460+roei97@users.noreply.github.com>
2024-05-27 21:52:20 +0300

    Delete שלב א directory

--- שלב א/dropTables.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE course_semester;
-DROP TABLE student_test_participation;
-DROP TABLE course_teacher;
-DROP TABLE semester;
-DROP TABLE grade;
-DROP TABLE test;
-DROP TABLE course;
-DROP TABLE teacher;
-DROP TABLE students;
