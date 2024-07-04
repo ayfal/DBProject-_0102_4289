b328fcd3a53fe9a0a5b010d23f04c175257c04ba
ayfal <65089103+ayfal@users.noreply.github.com>
2024-07-05 01:38:13 +0300

    pushing is now optional

--- git_sync.bat
+++ git_sync.bat
@@ -6,17 +6,23 @@ git pull
 
 REM Check if the pull was successful
 IF %ERRORLEVEL% EQU 0 (
-    REM Stage all changes
-    git add -A
+    REM ask the user if they want to push their changes
+    set /p push_changes="Do you want to push your changes? (y/n): "
 
-    REM Ask the user for the commit message
-    set /p commit_message="Enter commit message: "
-    
-    REM Commit the changes with the user-provided message
-    git commit -m "!commit_message!"
-    
-    REM Push any local changes to the remote repository
-    git push
+    REM if they do, stage, commit, and push the changes
+    IF /I "!push_changes!"=="y" (
+        REM Stage all changes
+        git add -A
+
+        REM Ask the user for the commit message
+        set /p commit_message="Enter commit message: "
+        
+        REM Commit the changes with the user-provided message
+        git commit -m "!commit_message!"
+        
+        REM Push any local changes to the remote repository
+        git push
+    )
 )
 
 REM Pause the script to see the output
