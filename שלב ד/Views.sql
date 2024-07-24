1d52b507c6ea7c66afb99d09fe120390c36f8726
ayfal <65089103+ayfal@users.noreply.github.com>
2024-07-24 16:38:46 +0300

    added 2 more queries to the views, and completed the report

--- שלב ד/Views.sql
+++ שלב ד/Views.sql
@@ -1,12 +1,18 @@
-CREATE VIEW grade_view as SELECT students.id, students.first_name,students.students, grade.grade, grade.test_id
+CREATE VIEW grade_view as SELECT students.id, students.first_name, students.last_name , grade.grade, grade.test_id FROM students, grade WHERE students.id = grade.student_id;
 
 -- גליון ציונים של סטודנט מסויים
-SELECT * FROM grade_view WHERE id =4
+SELECT * FROM grade_view WHERE id =4;
+
+-- ציוני כל הסטודנטים במבחן מסויים
+SELECT * FROM grade_view WHERE test_id = 3;
 
 
 
 
-CREATE VIEW tuition as SELECT students.id,students.first_name,students.last_name, student_transfers.student_id,bank_transfer.amount,bank_transfer.transfer_date,bank_transfer.description,bank_transfer.outgoing
+CREATE VIEW tuition as SELECT students.id, students.first_name, students.last_name, student_transfers.student_id, bank_transfer.amount, bank_transfer.transfer_date, bank_transfer.description, bank_transfer.outgoing FROM students, student_transfers, bank_transfer WHERE students.id = student_transfers.student_id AND student_transfers.transfer_id = bank_transfer.transfer_id;
 
 -- דוח תשלומי שכר לימוד של סטודנט
-SELECT * FROM `tuition` WHERE id = 4 AND outgoing = 1;
+SELECT * FROM tuition WHERE id = 60000 AND outgoing = 0;
+
+-- דוח תקבולים של שנה מסויימת
+SELECT * FROM tuition WHERE transfer_date BETWEEN '2020-01-01' AND '2020-12-31';
